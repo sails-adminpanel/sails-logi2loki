e2af484af568d2f6e976e2e3597904e7fa6bac5d
Xziy <xziy88@gmail.com>
2024-01-27 06:55:04 +0300

    no import

--- lib/initialize.ts
+++ lib/initialize.ts
@@ -1,5 +1,4 @@
 import afterHook from "./afterHook";
-import { customLogger } from "./prepareConfig"
 export default function ToInitialize(sails) {
 
   if (process.env.LOKI_CONFIG_HOST !== undefined) {
