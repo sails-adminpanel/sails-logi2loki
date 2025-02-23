ed7a698df15e55d8a341756696377ea3791638c6
Xziy <xziy88@gmail.com>
2025-02-23 07:20:11 +0300

    add webhook error push

--- lib/prepareConfig.ts
+++ lib/prepareConfig.ts
@@ -2,16 +2,16 @@ const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, colorize, label, printf, align } = format;
 const { SPLAT } = require('triple-beam');
 const { isObject } = require('lodash');
+const LokiTransport = require("winston-loki");
+const axios = require('axios');
+const os = require('os');
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
 
 function formatObject(param) {
-  if (isObject(param)) {
-    return JSON.stringify(param);
-  }
-  return param;
+  return isObject(param) ? JSON.stringify(param) : param;
 }
-
-
-const LokiTransport = require("winston-loki");
 
 // Function to format labels
 function formatLabel(labelName, value) {
@@ -19,54 +19,35 @@ function formatLabel(labelName, value) {
   return { [camelCaseLabelName]: value };
 }
 
-const os = require('os');
-const path = require('path');
-const crypto = require('crypto');
-
 const hostname = os.hostname();
 const cwd = process.cwd();
 const cwdHash = crypto.createHash('sha256').update(cwd).digest('hex');
 const last7Chars = cwdHash.substr(-7);
 
-const fs = require('fs');
-
-// Check if package.json exists
 let basename;
 try {
   const packageJson = fs.readFileSync('./package.json');
   const packageData = JSON.parse(packageJson);
   basename = packageData.name;
 } catch (error) {
-  // If package.json doesn't exist or couldn't be read, use the base name of cwd
   basename = path.basename(cwd);
 }
 
-// Set default value for LOKI_LABEL_APP_NAME if not provided
 const defaultAppName = process.env.APP_NAME ?? process.env.PROJECT_NAME ?? `${hostname}_${basename}_${last7Chars}`;
 process.env.LOKI_LABEL_APP_NAME = process.env.LOKI_LABEL_APP_NAME || defaultAppName;
-
 
 const env_labels = Object.entries(process.env)
   .filter(([key]) => key.startsWith("LOKI_LABEL_"))
   .map(([key, value]) => formatLabel(key.substring(11), value));
 
-/**
- * To add Loki labels, set the environment variable in the format LOKI_LABEL_NAME_LABEL=Value.
- * It will be converted to CamelCase format and used as a label for this Sails instance.
- * 
- * If LOKI_LABEL_APP_NAME is not explicitly set, it will be set by default in the following format:
- * AppName=`${hostname}_${basename_of_cwd}_${last_7_digits_of_sha256hash_cwd_path}`
- */
-
 const labels = {
   NODE_ENV: process.env.NODE_ENV,
-  ...Object.assign({}, ...env_labels) // Merge all label objects into one
-}
+  ...Object.assign({}, ...env_labels)
+};
 
-const headers = {}
-
-if(process.env.LOKI_ORG_ID) {
-  headers['X-Scope-OrgID'] = process.env.LOKI_ORG_ID
+const headers = {};
+if (process.env.LOKI_ORG_ID) {
+  headers['X-Scope-OrgID'] = process.env.LOKI_ORG_ID;
 }
 
 const _loki = new LokiTransport({
@@ -74,14 +55,12 @@ const _loki = new LokiTransport({
   interval: 5,
   labels: labels,
   ...process.env.LOKI_CONFIG_BASIC_AUTH && { basicAuth: process.env.LOKI_CONFIG_BASIC_AUTH },
-  // json: process.env.LOKI_CONFIG_JSON ? true : false,
-  // replaceTimestamp: true,
-  // format: process.env.LOKI_CONFIG_JSON ? format.json() : undefined,
   host: process.env.LOKI_CONFIG_HOST,
   ...Object.keys(headers).length > 0 && { headers: headers }
 });
 
-console.debug(`new loki instance initialize`)
+console.debug(`new loki instance initialize`, process.env.LOKI_LABEL_APP_NAME, `level: ${sails.config.log.level}`);
+console.debug(`loki: labels:`, labels);
 
 const all = format((info) => {
   const splat = info[SPLAT] || [];
@@ -91,22 +70,51 @@ const all = format((info) => {
   return info;
 });
 
+// Custom transport for webhook error reporting
+class HttpTransport extends transports.Stream {
+  constructor(opts) {
+    super(opts);
+    this.webhookUrl = opts.webhookUrl;
+  }
+
+  log(info, callback) {
+    setImmediate(() => this.emit('logged', info));
+    
+    if (info.level === 'error' && this.webhookUrl) {
+      axios.post(this.webhookUrl, {
+        level: info.level,
+        message: info.message,
+        timestamp: new Date().toISOString(),
+        labels,
+        meta: info.meta || {},
+      }).catch(err => {
+        console.error('Log sending error to Webhook:', err.message);
+      });
+    }
+
+    callback();
+  }
+}
+
+const transportsList = [
+  _loki,
+  new transports.Console({
+    format: combine(
+      all(),
+      label({ label: "version" }),
+      timestamp(),
+      colorize(),
+      align(),
+      printf(info => `${formatObject(info.message)}`)
+    )
+  })
+];
+
+if (process.env.ERROR_WEBHOOK_URL) {
+  transportsList.push(new HttpTransport({ webhookUrl: process.env.ERROR_WEBHOOK_URL }));
+}
+
 export const customLogger = createLogger({
-  //@ts-ignore
   level: sails.config.log.level,
-  // format: format.json(),
-  // levels: config.syslog.levels,
-  transports: [
-    _loki,
-    new transports.Console({
-      format: combine(
-        all(),
-        label({ label: "version" }),
-        timestamp(),
-        colorize(),
-        align(),
-        printf(info => `${formatObject(info.message)}`)
-      )
-    })
-  ],
+  transports: transportsList,
 });
